12af67a64617c98132769f711738eb5c9aea1453
gansior-ag <gansior-ag@rosseti.digital>
2021-11-25 09:26:07 +0300

    current work

---
+++ examples/recognition_text/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='people_recognition_simbol', # Имя приложения
+    version='0.0.1', # номер версии
+    packages=['creater_array_marked_char'], # Python пакет включен в установочный пакет
+)
