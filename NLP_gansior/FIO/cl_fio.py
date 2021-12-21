f95c3708b1c5b7b158180c4c845d4648758398fc
gansior <breslau1962@yandex.ru>
2021-12-21 20:21:29 +0300

    add changes VS code

---
+++ NLP_gansior/FIO/cl_fio.py
@@ -0,0 +1,17 @@
+"""
+This module make ricognition FIO
+
+Author Gansior Alexandr mail - gansior@gansior.ru tel - +79173383804
+"""
+from fio import FIO
+
+def proga():
+    fio = FIO()
+    print(fio.predict('Корнеев'))
+    print(fio.predict('Вдохновение'))
+    print(fio.predict('Вдохновляющий'))
+    print(fio.predict('Спенсер'))
+
+if __name__ == '__main__':
+    proga()
+    'ertewtew'.isdigit()
---
+++ NLP_gansior/professions/READMI.md
@@ -0,0 +1,3 @@
+# Данный раздел посвящен рспознаванию профессий как именнованную сущность в тексте для русского языка
+
+# This section is devoted to the recognition of professions as a named entity in the text for rus langage
