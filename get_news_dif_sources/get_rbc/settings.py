56f1e36225504fdf69dd733ad5403d1239396ebd
gansiorag <breslau1962@yandex.ru>
2023-12-04 07:22:30 +0300

    add ch

--- get_news_dif_sources/get_rbc/settings.py
+++ get_news_dif_sources/get_rbc/settings.py
@@ -4,4 +4,4 @@
 # And then replace the link here:
 
 
-url = 'https://www.rbc.ru/search/?query=%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8F+%D1%81%D0%B8%D1%80%D0%B8%D1%8F+%D0%B2%D0%BE%D0%B9%D0%BD%D0%B0&project=rbcnews&dateFrom=01.05.2016&dateTo=31.08.2016'
+url = 'https://www.rbc.ru/search/?query=%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8F+%D1%81%D0%B8%D1%80%D0%B8%D1%8F+%D0%B2%D0%BE%D0%B9%D0%BD%D0%B0&project=rbcnews&dateFrom=01.12.2023&dateTo=04.12.2023'
